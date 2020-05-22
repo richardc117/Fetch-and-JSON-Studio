30955bf219457fe0f492d2f1ff0e28421468428c
Richard Carino <richard.carino17@gmail.com>
2020-05-23 04:41:37 +1000

    Debugging why it wont load first part (1)

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 // TODO: add code here
 
-window.addEventListener("load", ()=>{
-    fetch
-    ("https://handlers.education.launchcode.org/static/astronauts.json)").then(function(response){
-        console.log(response);
+window.addEventListener("load", function(){
+    fetch("https://handlers.education.launchcode.org/static/astronauts.json)").then(function(response){
+        console.log(response.json());
         // //Get each element in the array from the website
         // for(i=0; i<response.length; i++){
         //     response[i]    
