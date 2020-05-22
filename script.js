43341e59fda9233176eb0434d624d6c08d95c7bd
Richard Carino <richard.carino17@gmail.com>
2020-05-23 05:33:16 +1000

    Got the code to push astronaut list dynamically

--- script.js
+++ script.js
@@ -4,7 +4,12 @@ window.addEventListener("load", function(){
     let url = "https://handlers.education.launchcode.org/static/astronauts.json"
     fetch(url).then(function(response) {
         response.json().then( function(json) {
-            console.log(json);
+            const list = document.getElementById("container");
+            list.innerHTML = "<ul><br>"
+            for(i=0; i < json.length; i++){
+                list.innerHTML += `<li> ${json[i].firstName} ${json[i].lastName} </li>`
+            }
+            list.innerHTML += "</ul>"
         })
     })
 });
