3056a59394d40ce222a4b9a5115d175eba0f67b2
Richard Carino <richard.carino17@gmail.com>
2020-05-23 04:38:59 +1000

    Added on load script

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 // TODO: add code here
+
+window.addEventListener("load", ()=>{
+    fetch
+    ("https://handlers.education.launchcode.org/static/astronauts.json)").then(function(response){
+        console.log(response);
+        // //Get each element in the array from the website
+        // for(i=0; i<response.length; i++){
+        //     response[i]    
+        // }
+    })
+})
