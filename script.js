e90e078ea6c3252ad757e62233e15ce2493e57ea
Richard Carino <richard.carino17@gmail.com>
2020-05-23 05:18:38 +1000

    Fixed the bug where it couldnt fetch

--- script.js
+++ script.js
@@ -1,18 +1,10 @@
 // TODO: add code here
 
 window.addEventListener("load", function(){
-    console.log("window loaded");
-    fetch("https://handlers.education.launchcode.org/static/astronauts.json)").then(function(response) {
-    // response.json().then( function(json) {
-    //     console.log(response);
-        console.log("fetch worked");
-    //}
-    //fetch("https://handlers.education.launchcode.org/static/weather.json").then(function(response) {
-   //console.log(response);
-
-        // //Get each element in the array from the website
-        // for(i=0; i<response.length; i++){
-        //     response[i]    
-        // }
+    let url = "https://handlers.education.launchcode.org/static/astronauts.json"
+    fetch(url).then(function(response) {
+        response.json().then( function(json) {
+            console.log(json);
+        })
     })
 });
